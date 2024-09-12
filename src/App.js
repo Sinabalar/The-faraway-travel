0a2643a3824607afeb8507de1530eb44e78a2515
Sinabalar <sinabalar666@gmail.com>
2024-09-12 16:01:09 +0330

    Initial commit: Basic structure of the travel packing app with Logo, Form, PackingList, and Stats components.

---
+++ src/App.js
@@ -0,0 +1,38 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+function Logo() {
+  return (
+    <h1>🏝 Far Away 🛄</h1>
+  );
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for you'r 😍 trip ?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      LIST
+    </div>
+  );
+};
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        💼 You have X items on your list and you already packed X (X%)
+      </em>
+    </footer>
+  );
+}
