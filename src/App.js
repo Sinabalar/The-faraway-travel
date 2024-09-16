161c39f145de44b1010f25d6a96dbba35c97656a
Sinabalar <sinabalar666@gmail.com>
2024-09-16 16:01:42 +0330

    Add item management functionality: Implemented useState for form handling, added initial items, and created Item component for packing list.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 3, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
+
+
 export default function App() {
   return (
     <div className="app">
@@ -14,16 +23,44 @@ function Logo() {
   );
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function HandelForm(e) {
+    e.preventDefault();
+
+    if (description === "") return;
+
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false
+    }
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  };
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={HandelForm}>
       <h3>What do you need for you'r 😍 trip ?</h3>
-    </div>
+      <select value={quantity} onChange={event => setQuantity(Number(event.target.value))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+      </select>
+      <input type="text" placeholder="Item..." value={description} onChange={event => setDescription(event.target.value)} />
+      <button> add</button>
+    </form>
   );
 }
 function PackingList() {
   return (
     <div className="list">
-      LIST
+      <ul>
+        {
+          initialItems.map(el => <Item item={el} key={el.id} />)
+        }
+      </ul>
     </div>
   );
 };
@@ -34,5 +71,15 @@ function Stats() {
         💼 You have X items on your list and you already packed X (X%)
       </em>
     </footer>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li >
   );
 }
